23f546ba5292648ff6d6810dd3de3470db86e80d
Martin Cross <mncross@gmail.com>
2016-02-12 14:58:14 -0600

    completed AboutNil.cs

--- CSharp/AboutNil.cs
+++ CSharp/AboutNil.cs
@@ -7,7 +7,7 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void NilIsNotAnObject()
         {
-            Assert.True(typeof(object).IsAssignableFrom(null)); //not everything is an object
+            Assert.False(typeof(object).IsAssignableFrom(null)); //not everything is an object
         }
 
         [Koan(2)]
@@ -17,8 +17,8 @@ namespace DotNetKoans.CSharp
             //Don't be confused by the code below. It is using Anonymous Delegates which we will
             //cover later on. 
             object nothing = null;
-            Assert.Throws(typeof(FillMeIn), delegate() { nothing.ToString(); });
-
+            Assert.Throws(typeof(System.NullReferenceException), delegate() { nothing.ToString(); });
+                /* System.NotSupportedException why was this an answer? */
             //What's the message of the exception? What substring or pattern could you test
             //against in order to have a good idea of what the string is?
             try
@@ -27,7 +27,7 @@ namespace DotNetKoans.CSharp
             }
             catch (System.Exception ex)
             {
-                Assert.Contains(FILL_ME_IN as string, ex.Message);
+                Assert.Contains("Object reference" as string, ex.Message);
             }
         }
 
@@ -35,21 +35,26 @@ namespace DotNetKoans.CSharp
         public void CheckingThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj == FILL_ME_IN);
+            Assert.True(obj == null);
         }
 
         [Koan(4)]
         public void ABetterWayToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.Null(FILL_ME_IN);
+            Assert.Null(obj);
         }
 
         [Koan(5)]
         public void AWayNotToCheckThatAnObjectIsNull()
         {
+            /*
+            I'm not sure that I understand the concept that this Koan is trying to teach me.
+            Is my solution to this Koan bad practice? If so, why? Why is the solution to the previous Koan better?
+            */
             object obj = null;
-            Assert.True(obj.Equals(null));
+            Assert.True(obj == null);
+            //Assert.True(obj.Equals(null));
         }
     }
 }
