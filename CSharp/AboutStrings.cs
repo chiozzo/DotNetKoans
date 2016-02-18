b6e650078fa1b1d949b4f14902baf1ef02ebac86
Martin Cross <mncross@gmail.com>
2016-02-18 17:20:15 -0600

    completed Koan 29

--- CSharp/AboutStrings.cs
+++ CSharp/AboutStrings.cs
@@ -114,8 +114,8 @@ broken line";
             var strA = "Hello, ";
             var strB = "World";
             strA += strB;
-            Assert.Equal(FILL_ME_IN, strA);
-            Assert.Equal(FILL_ME_IN, strB);
+            Assert.Equal("Hello, World", strA);
+            Assert.Equal("World", strB);
         }
 
         [Koan(11)]
