c33c36a52446e006dae310cf4744a79eb8d938dc
Martin Cross <mncross@gmail.com>
2016-02-22 13:36:39 -0600

    completed Koan 48

--- CSharp/AboutStrings.cs
+++ CSharp/AboutStrings.cs
@@ -313,7 +313,7 @@ broken line";
         {
             var str = "Sausage Egg Cheese";
             string[] words = str.Split();
-            Assert.Equal(new[] { FILL_ME_IN }, words);
+            Assert.Equal(new[] { "Sausage", "Egg", "Cheese" }, words);
         }
 
         [Koan(32)]
